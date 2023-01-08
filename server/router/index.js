e180233e943bea0808ef29e0a18c9043e565b32b
Mitya <partyzan327@gmail.com>
2023-01-08 14:30:57 +0300

    feat: define userController

--- server/router/index.js
+++ server/router/index.js
@@ -1,12 +1,13 @@
 const { Router } = require('express');
+const userController = require('../controller/UserController');
 
 const router = new Router();
 
-router.post('/registration');
-router.post('/login');
-router.post('/logout');
-router.get('/activate/:link');
-router.get('/refresh');
-router.get('/users');
+router.post('/registration', userController.registration);
+router.post('/login', userController.login);
+router.post('/logout', userController.logout);
+router.get('/activate/:link', userController.activate);
+router.get('/refresh', userController.refresh);
+router.get('/users', userController.getAllUsers);
 
 module.exports = router;
