92dea33195c488e4a7a6c6ba54f333acb9e5f2ff
Mitya <partyzan327@gmail.com>
2023-01-08 14:01:12 +0300

    feat: connect DB

--- server/index.js
+++ server/index.js
@@ -1,13 +1,23 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-require('dotenv').config();
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3001;
+const mongoURL = process.env.MONGO_URL;
+
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
 
 const start = async () => {
     try {
+        mongoose.set({ strictQuery: false });
+        mongoose.connect(mongoURL);
+
         app.listen(PORT, () =>
             console.log(`Server started at http://localhost:${PORT}`)
         );
