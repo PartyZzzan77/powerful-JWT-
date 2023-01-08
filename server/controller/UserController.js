9fca64b07987b298a2285652501c389e0db59773
Mitya <partyzan327@gmail.com>
2023-01-08 16:13:55 +0300

    feat: set cookie refreshToken

--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -3,7 +3,17 @@ const userService = require('../service/UserService');
 class UserController {
     async registration(req, res, next) {
         try {
-        } catch (error) {}
+            const { email, password } = req.body;
+            const userData = await userService.registration(email, password);
+
+            res.cookie('refreshToken', userData.refreshToken, {
+                maxAge: 30 * 24 * 60 * 60 * 1000,
+                httpOnly: true,
+            });
+            return res.json(userData);
+        } catch (error) {
+            console.log(error.message);
+        }
     }
 
     async login(req, res, next) {
