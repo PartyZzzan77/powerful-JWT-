d55fc28fd00abc12e5eca71a6fee7d7090ef4170
Mitya <partyzan327@gmail.com>
2023-01-09 14:35:30 +0300

    feat: implement LoginForm component

---
+++ client/src/components/LoginForm.tsx
@@ -0,0 +1,27 @@
+import { FC, useState } from 'react';
+
+const LoginForm: FC = () => {
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+
+    return (
+        <div>
+            <input
+                type='text'
+                placeholder='Email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+                type='text'
+                placeholder='Password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button>Login</button>
+            <button>Registration</button>
+        </div>
+    );
+};
+
+export default LoginForm;
